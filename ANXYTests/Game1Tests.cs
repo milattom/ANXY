26ef78382de85b8dd771cabf78728e36f0dce2d2
Tomas Stefan Milata <milattom@students.zhaw.ch>
2022-10-02 14:31:35 +0200

    Move Git 2

---
+++ ANXYTests/Game1Tests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ANXY;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ANXY.Tests
+{
+    [TestClass()]
+    public class Game1Tests
+    {
+        [TestMethod()]
+        public void Game1Test()
+        {
+            Assert.Fail();
+        }
+    }
+}
