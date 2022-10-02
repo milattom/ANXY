c3d7ad74f66cc9e68b45e46488609efe915b651a
Tomas Stefan Milata <milattom@students.zhaw.ch>
2022-10-02 14:04:04 +0200

    move git folder one up

---
+++ ANXYTests/Game1Tests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace ANXY.Tests
+{
+    [TestClass]
+    public class Game1Tests
+    {
+        [TestMethod]
+        public void Game1Test()
+        {
+            Console.WriteLine("first unit test");
+            Game1 game1 = new Game1();
+            Assert.IsNotNull(game1);
+        }
+    }
+}
