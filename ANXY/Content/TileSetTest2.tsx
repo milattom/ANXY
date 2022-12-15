0c9f5a8e7a698a6c4b1f435987d720af10a744df
milattom <milattom@students.zhaw.ch>
2022-12-15 01:22:34 +0100

    Add Tiled Level to Entity Logic
    Import Tiled Level into the Game.
    Translate imported Tiled Level to Entities.
    Add BoxColliders for Level Tiles.
    Add initial Camera
    Minor Refactoring
    
    ANXY-28

--- ANXY/Content/TileSetTest2.tsx
+++ ANXY/Content/TileSetTest2.tsx
@@ -1,7 +1,18 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="TileSetTest2" tilewidth="32" tileheight="32" tilecount="247" columns="19">
- <image source="../../Tiled/TileSet2.png" width="608" height="416"/>
+ <image source="TileSet2.png" width="608" height="416"/>
+ <tile id="20">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.136364" y="0.0909091" width="9.59091" height="31.9545"/>
+   <object id="5" x="4.90909" y="0.272727" width="26.9091" height="8"/>
+  </objectgroup>
+ </tile>
  <tile id="96">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="100">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
---
+++ ANXY/EntityComponent/Components/Camera.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ANXY.EntityComponent.Components
+{
+    public class Camera : Component
+    {
+        public override void Update(GameTime gameTime)
+        {
+            //TODO
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            //TODO
+        }
+
+        public override void Initialize()
+        {
+            //TODO
+        }
+
+        public override void Destroy()
+        {
+            //TODO
+        }
+    }
+}
