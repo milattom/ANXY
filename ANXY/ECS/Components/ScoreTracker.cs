76a6df6ff7c29ce7b08051e2beb2578e190d8090
Dominic Akeret <akeredom@students.zhaw.ch>
2023-06-12 21:08:01 +0200

    moved scoretracker back to component

--- ANXY/ECS/Components/ScoreTracker.cs
+++ ANXY/ECS/Components/ScoreTracker.cs
@@ -1,17 +1,19 @@
 ﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using System;
 
-namespace ANXY.Start;
+namespace ANXY.ECS.Components;
 
 /// <summary>
 /// TODO implement ScoreTracker to track HighScore, personal Score, etc
 /// Maybe track time as score?
 /// </summary>
-public class ScoreTracker
+public class ScoreTracker : Component
 {
     public int Time { get; set; }
     public int Score { get; set; }
 
     // TODO public File HighScore;
+    public override void Update(GameTime gameTime)
+    {
+        //
+    }
 }
